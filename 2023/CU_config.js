748e39da9bb9aaebcc704c997346d88b2415cca6
Lunar-Academy <69018644+Lunar-Academy@users.noreply.github.com>
2024-01-07 20:40:58 -0500

    Frontend Copied and slightly modified from last year's app

---
+++ 2023/CU_config.js
@@ -0,0 +1,19 @@
+var config_data = `
+{
+  "title": "",
+  "page_title": "",
+  "checkboxAs": "10",
+  "prematch": [
+
+  ],
+  "auton": [
+  ],
+  "teleop": [
+   
+
+  ],
+  "endgame": [
+  ],
+  "postmatch": [
+  ]
+}`;
